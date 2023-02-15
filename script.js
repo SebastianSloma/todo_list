da5fe7f4caee4ba3d103ab2904358dccc793155d
Sebastian <sebastiansloma@gmail.com>
2023-02-15 15:24:08 +0100

    A function has been added that adds new tasks

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let $todoInput;
 let $alertInfo;
 let $addBtn;
 let $ulList;
+let $newTask;
 
 const main = () => {
 	prepareDOMElements();
@@ -15,6 +16,21 @@ const prepareDOMElements = () => {
 	$ulList = document.querySelector('.todoList ul');
 };
 
-const prepareDOMEvents = () => {};
+const prepareDOMEvents = () => {
+	$addBtn.addEventListener('click', addNewTask);
+};
+
+const addNewTask = () => {
+	if ($todoInput.value !== '') {
+		$newTask = document.createElement('li');
+		$newTask.innerText = $todoInput.value;
+		$ulList.appendChild($newTask);
+
+        $todoInput.value = ''
+        $alertInfo.innerText = ''
+	} else {
+		$alertInfo.innerText = 'Enter the content of the task...';
+	}
+};
 
 document.addEventListener('DOMContentLoaded', main);
