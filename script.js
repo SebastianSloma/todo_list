1879fbc78be363c2403a4697d51e492bcf928fc9
Sebastian <sebastiansloma@gmail.com>
2023-02-15 15:37:08 +0100

    added tools panel to task

--- script.js
+++ script.js
@@ -26,11 +26,34 @@ const addNewTask = () => {
 		$newTask.innerText = $todoInput.value;
 		$ulList.appendChild($newTask);
 
-        $todoInput.value = ''
-        $alertInfo.innerText = ''
+		$todoInput.value = '';
+		$alertInfo.innerText = '';
+		createToolsArea();
 	} else {
 		$alertInfo.innerText = 'Enter the content of the task...';
 	}
+};
+
+const createToolsArea = () => {
+	const toolsPanel = document.createElement('div');
+	toolsPanel.classList.add('tools');
+	$newTask.appendChild(toolsPanel);
+
+	const completeBtn = document.createElement('button');
+	completeBtn.classList.add('complete');
+	completeBtn.innerHTML = '<i class="fas fa-check"></i>';
+
+	const editBtn = document.createElement('button');
+	editBtn.classList.add('edit');
+	editBtn.innerText = 'EDIT';
+
+	const deleteBtn = document.createElement('button');
+	deleteBtn.classList.add('delete');
+	deleteBtn.innerHTML = '<i class="fas fa-times"></i>';
+
+	toolsPanel.appendChild(completeBtn);
+	toolsPanel.appendChild(editBtn);
+	toolsPanel.appendChild(deleteBtn);
 };
 
 document.addEventListener('DOMContentLoaded', main);
